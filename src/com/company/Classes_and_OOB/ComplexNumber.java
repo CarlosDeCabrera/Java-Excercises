cfccb1085769142994f58d68520304e561106fa6
Aeragorne <decabreracarlos@gmail.com>
2022-07-22 14:54:43 -0400

    Complex number

---
+++ src/com/company/Classes_and_OOB/ComplexNumber.java
@@ -0,0 +1,38 @@
+package com.company.Classes_and_OOB;
+
+public class ComplexNumber {
+
+    private double real;
+    private double imaginary;
+
+
+    public ComplexNumber(double real, double imaginary){
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+
+    public double add (double real, double imaginary){
+        return -1;
+    }
+
+    public double add (ComplexNumber complexNumber){
+        return -1;
+    }
+
+    public double subtract (double real, double imaginary){
+        return -1;
+    }
+    public double subtract (ComplexNumber complexNumber){
+        return -1;
+    }
+
+
+}
